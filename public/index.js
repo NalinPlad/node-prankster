46e3339408923ae0aa92725ae9f78971f7ebf84c
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-05-20 15:20:00 -0700

    ☺️ scary auto reload

--- public/index.js
+++ public/index.js
@@ -1,23 +1,47 @@
 con = document.getElementById('con_stat');
+out = document.getElementById('out');
 
+con.classList.remove('con_green')
+con.classList.remove('con_red')
 con.classList.add('con_yellow')
 con.innerText = 'Status:    Connecting...';
 
-socket = new WebSocket('ws://'  + window.location.hostname + ':3001')
+function rlWs() {
+    socket = new WebSocket('ws://'  + window.location.hostname + ':3001')
 
-out = document.getElementById('out');
+    socket.addEventListener('open', function (event) {
+        socket.send('hello');
+        ping();
+        setInterval(ping, 1000);
+    });
+    
+    socket.addEventListener('message', function (event) {
+        if (event.data == '__pong__') {
+            pong();
+            return;
+        }
+    
+        const now = new Date();
+        const current = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+        out.innerHTML += "<br/><b class='serv_text'>[SERVER][" + current + "] ===></b> " + event.data;
+    })
+
+}
 
 // Ping Pong
 function ping() {
     socket.send('__ping__');
-    tm = setTimeout(function () {
+    tm = setTimeout(_ => {
 
         con.classList.remove('con_yellow')
         con.classList.remove('con_green')
         con.classList.add('con_red')
         con.innerText = 'Status:    Not Connected...'
+        setTimeout(_ => {
+            rlWs()
+        },3000)
 
-}, 5000);
+}, 1000);
 }
 
 function pong() {
@@ -28,24 +52,8 @@ function pong() {
     con.innerText = 'Status:    Connected'
 }
 
+rlWs();
 
-socket.addEventListener('open', function (event) {
-    socket.send('hello');
-    ping();
-    setInterval(ping, 3000);
-});
-
-socket.addEventListener('message', function (event) {
-    console.log(event.data);
-    if (event.data == '__pong__') {
-        pong();
-        return;
-    }
-
-    const now = new Date();
-    const current = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-    out.innerHTML += "<br/><b class='serv_text'>[SERVER][" + current + "] ===></b> " + event.data;
-})
 
 function send(data){
     socket.send(data);
